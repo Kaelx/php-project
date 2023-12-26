528d268bd14bfc3986354ab9239405f66c24feaa
kaelx <johnmark.cuyos@evsu.edu.ph>
2023-12-26 13:51:27 +0800

    update

--- banned.php
+++ banned.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WARNING</title>
     <link rel="stylesheet" href="style/bootstrap.min.css">
 </head>
+
 <body>
 
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card text-white bg-danger mb-3">
-                <div class="card-body">
-                    <h1 class="text-center">Access Denied!</h1>
-                    <p class="text-center">You have been banned for violating our terms of service.</p>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card text-white bg-danger mb-3">
+                    <div class="card-body">
+                        <h1 class="text-center">Access Denied!</h1>
+                        <p class="text-center">You have been banned for violating our terms of service.</p>
+                    </div>
                 </div>
-            </div>
-            <div class="text-center mt-5">
-                <a href="index.php" class="btn btn-warning">RETURN HOME</a>
+                <div class="text-center mt-5">
+                    <a href="index.php" class="btn btn-warning">RETURN HOME</a>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 </body>
+
 </html>
