110c355d2887e330326c32516f381a550069eb00
kaelx <johnmark.cuyos@evsu.edu.ph>
2023-12-31 10:45:59 +0800

    update

--- update.php
+++ update.php
@@ -60,7 +60,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Farmers Assistant Web Service - Update Profile</title>
+    <title>Update Profile</title>
     <link rel="icon" href="storage/farmer.png" type="image/x-icon">
     <link rel="stylesheet" href="style/bootstrap.min.css">
     <link rel="stylesheet" href="style/profile.css">
